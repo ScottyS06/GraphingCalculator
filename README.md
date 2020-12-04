8b38e2375078ecfc74b4748770d15479d82ca4e2
akashgujjar <akashsgujjar@gmail.com>
2020-12-03 17:32:13 -0800

    final commit

--- README.md
+++ README.md
@@ -2,4 +2,30 @@
 This is a graphing calculator python web application. 
 The application features a user interface that allows the client to choose between a set of function types.
 The user can then enter the corresponding coefficient and constant values for the function and see it graphed on a 
-set domain. This program leverages certain python libraries like matplotlib and flask.
+set domain. This program leverages certain Python libraries like MatPlotLib and Flask.
+
+# How to start
+In the terminal paste "FLASK_APP=app.py flask run" and hit enter. Then go to the network tab in the upper right corner of Ed 
+and select the network to open up the application. You will automatically be directed to a new tab with the Graphinator 
+application open.
+
+# How to use
+Once the application opens you will see an Html page with several input fields. To start off using the graphing web app you 
+must select the type of function you want to be graphed from the drop down. Graphinator features 3 types of functions: linear, quadratic, and 
+cubic. Once you select a function type, the Html page will display several other input fields. These fields correspond to the 
+coefficient values associated with the different terms for the selected function. For example, if you select the linear 
+function type you will see input fields for the slope and the y-intercept. Our application also features a smart scale 
+function. The user will be able to select whether they want this feature enabled for the current function they are graphing. If 
+selected the function will be graphed on a y-scale that fits the function proportionately. Otherwise, the function will be 
+displayed on a preset scale that goes from y = -30 to y = 30. Once all the input fields have been filled out, the user can hit 
+the graph function and an image of the function will be generated and displayed on the Html page. To graph other functions, the 
+user must follow the previous steps but apply new values to the different input fields.
+
+# Important Links
+# Github Repo
+https://github.com/ScottyS06/GraphingCalculator
+# ED Workspace
+https://us.edstem.org/courses/631/workspaces/pFHnpvYslOxEjlmr1aZ5aj7GoyFr2cNe
+# User Video
+https://www.loom.com/share/1782e84fa4cf4e6c90a070c833723c03?sharedAppSource=personal_library 
+
--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 from matplotlib import pyplot as plt
 
 matplotlib.use('Agg')
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
@@ -13,19 +12,21 @@ minY = 0
 maxY = 0
 smartScale = "yes"
 
-
+# home page for web application
 @app.route('/')
-def hello_world():
-    return render_template('index.html', name="test")
+def home():
+    return render_template('index.html')
 
-
+# establishs the domain [-10, 10] for which 
+# coresponding Y values are computed
 def xvalueAdd():
     start = -10
-    for x in range(640):
+    for x in range(700):
         xVals.append(start)
         start += .03
 
-
+# called when the user enter's a graphable filter
+# collects data from the user based on which fucntion they want to graph
 @app.route('/graph', methods=['POST'])
 def graph():
     plt.clf()
@@ -45,9 +46,33 @@ def graph():
         c = request.form['c']
         quadratic(a, b, c)
         return render_template('index.html', a=a, b=b, c=c, error=function)
+    elif function == "cubic":
+        a = request.form['ca']
+        b = request.form['cb']
+        c = request.form['cc']
+        d = request.form['cd']
+        cubic(a, b, c, d)
+        return render_template('index.html', a=a, b=b, c=c, d=d, error=function)
     return render_template('index.html', error=function)
 
+# computes Y values from the domain [-10, 10] for
+# all linear functions
+def linear(slope, y_int):
+    global maxY
+    global minY
+    maxY = 0
+    minY = 0
+    for value in xVals:
+        yValue = value * int(slope)
+        if yValue > maxY:
+            maxY = yValue
+        if yValue < minY:
+            minY = yValue
+        yVals.append(yValue + int(y_int))
+    graphing()
 
+# computes Y values from the domain [-10, 10] for
+# all quadratic functions
 def quadratic(a, b, c):
     global maxY
     global minY
@@ -64,41 +89,47 @@ def quadratic(a, b, c):
         yVals.append(yValue)
     graphing()
 
-
-def clearAll():
-    xVals.clear()
-    yVals.clear()
-
-
-def linear(slope, y_int):
+# computes Y values from the domain [-10, 10] for
+# all cubic functions
+def cubic(a, b, c, d):
     global maxY
     global minY
     maxY = 0
     minY = 0
+    print(a)
+    print(type(a))
     for value in xVals:
-        yValue = value * int(slope)
+        yValue = (value ** 3) * int(a)
+        yValue += value * value * int(b)
+        yValue += value * int(c)
+        yValue += int(d)
         if yValue > maxY:
             maxY = yValue
         if yValue < minY:
             minY = yValue
-        yVals.append(yValue + int(y_int))
+        yVals.append(yValue)
     graphing()
 
+# clears all points from previous entry
+def clearAll():
+    xVals.clear()
+    yVals.clear()
 
+# plots all of the points that are computed and stored
+# allows users to pick a dynamically scaled range to graph Y values
+# saves an image file for the coresponding graph
 def graphing():
     plt.scatter(xVals, yVals)
     plt.grid(True)
     plt.autoscale(False)
     plt.xlim((-10, 10))
     if smartScale == "yes":
-
         plt.ylim(minY * 1.1, maxY * 1.1)
     else:
         plt.ylim(-30, 30)
-
     plt.savefig('static/graph.png')
 
-
+# manually disables caching of all image files and data on between each use
 @app.after_request
 def add_header(response):
     response.cache_control.no_store = True
@@ -107,4 +138,3 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-# FLASK_APP=app.py FLASK_ENV=development flask run
